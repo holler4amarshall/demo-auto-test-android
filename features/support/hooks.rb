0944c938a5b143e7bd4c9fb09c75057544cae70e
holler4amarshall <holler4amarshall@hotmail.com>
2016-09-19 20:30:42 +1000

    add ability to take screenshot on scenario failure

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -3,7 +3,25 @@ Before do
 	$driver.start_driver
 end
 
-After do
-	#sleep  5
+After do |scenario|
+	#screenshots functionality
+	#create directory for the screenshots if it doesn't exist
+	if scenario.failed?
+		unless File.directory?("screenshots")
+			FileUtils.mkdir_p("screenshots")
+		end
+	#create new file for screenshot, named with current date/time
+	time_stamp = Time.now.strftime("%Y-%m-%d-_%H.%M.%S")
+	screenshot_name = "#{time_stamp}.png"
+	screenshot_file = File.join("screenshots", screenshot_name)
+
+	#take screenshot
+	$driver.screenshot(screenshot_file)
+	embed("screenshots", screenshot_file, "image/png")
+	end
 	$driver.driver_quit
 end
+
+AfterConfiguration do
+	FileUtils.rm_r("screenshots") if File.directory?("screenshots")
+	end
