a2c55c43389250f9a3c49dc52fb8ce8af40b89ee
holler4amarshall <holler4amarshall@hotmail.com>
2016-09-19 22:01:53 +1000

    add pry gem for step definition debugging

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,12 +1,13 @@
 require "appium_lib"
 require "page-object"
+require "pry" #used for debugging
 
 def caps 
 	{
 		caps: {
 			deviceName: "Samsung",
 			platformName: "Android",
-			app: "/<path to app>/SeriesGuide-31.1.apk",
+			app: "<path to test project>/demo-auto-test-android/features/support/SeriesGuide-31.1.apk",
             appPackage: "com.battlelancer.seriesguide",
             appActivity: ".ui.ShowsActivity",
 			newCommandTimeout: "3600",
