78cf4226153b5ce8fb8c8f27eb45c48b0a5d976f
holler4amarshall <holler4amarshall@hotmail.com>
2016-09-19 21:16:00 +1000

    add step definitions to handle swipe gesture

---
+++ features/step_definitions/gestures_steps.rb
@@ -0,0 +1,7 @@
+When(/^I swipe from left to right$/) do
+  Appium::TouchAction.new.swipe(start_x: 0.01, start_y: 0.5, end_x: 0.5, end_y: 0.5, duration: 800).perform
+end
+
+When(/^I swipe from right to left$/) do
+  Appium::TouchAction.new.swipe(start_x: 0.99, start_y: 0.5, end_x: 0.5, end_y: 0.5, duration: 800).perform
+end
